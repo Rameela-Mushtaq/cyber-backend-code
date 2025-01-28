f557950c9dab9a576bb8ffb45f742d00e2ae7bb1
Rameela-Mushtaq <rameelamushtaq2gmail.com>
2025-01-28 18:59:55 +0500

    index

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
     cors({
       origin: [
-        "http://localhost:3000",
+        //"http://localhost:3000",
         "https://cyber-frontend-gamma.vercel.app",
       ],
       credentials: true,
